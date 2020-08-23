f6eef55b371028af92422d059acf03db8958369a
n0nag0n <acollier21@gmail.com>
2020-08-23 07:58:08 -0600

    fixed unit tests to have proper formatting and color

--- templates/unit-test.php
+++ templates/unit-test.php
@@ -25,17 +25,63 @@ function echoLine(string $text) {
 	}
 }
 
+function formatTextColor($str, $color) {
+	switch($color) {
+		case "green":
+			$return = "\033[0;32m{$str}\033[0m";
+			break;
+			
+		case "red":
+			$return = "\033[0;31m{$str}\033[0m";
+			break;
+			
+		case "yellow":
+			$return = "\033[1;33m{$str}\033[0m";
+			break;
+			
+		case "white":
+			$return = "\033[1;37m{$str}\033[0m";
+			break;
+			
+		case "orange":
+			$return = "\033[0;33m{$str}\033[0m";
+			break;
+			
+		case "blue":
+			$return = "\033[0;34m{$str}\033[0m";
+			break;
+			
+		case "gray":
+		case "grey":
+			$return = "\033[1;30m{$str}\033[0m";
+			break;
+			
+		default:
+			$return = $str;
+	}
+	
+	return $return;
+}
+
+function prompt($prompt, $strtolower = true) {
+	echoLine($prompt.' ', 'green');
+	$response = trim(fgets(STDIN));
+	if($strtolower)
+		$response = strtolower($response);
+	return $response;
+}
+
 $test = new Test;
 
 echoLine('Beginning Unit Tests');
 $source_time_stamps = [];
-foreach(glob(__DIR__.'/../../{{ @cnf_tests }}/*') as $unit_test_file) {
+foreach(glob(__DIR__.'/../../{{ @cnf_tests }}*') as $unit_test_file) {
 	if($unit_test_file === '.' || $unit_test_file === '..') {
 		continue;
 	}
 	$start_ts = getCurrentMicrotime();
 	$filename = basename($unit_test_file);
-	echoLine($ts.' - Testing '.$filename);
+	echoLine('Testing '.$filename);
 	require($unit_test_file);
 	$source_time_stamps[$filename] = calculateMicrotimeDifference($start_ts,getCurrentMicrotime());
 }
@@ -44,12 +90,12 @@ echoLine('Completed Unit Tests');
 foreach ($test->results() as $result) {
 	$desc = '';
 	if(!empty($result['text'])) {
-		$desc = $result['text'].' - ';
+		$desc = ' - '.$result['text'];
 	}
-	$filename = basename($result['source']);
+	$filename = basename(explode(':', $result['source'])[0]);
     if($result['status']) {
-        echoLine($filename.' - '.$desc.'Pass ('.$source_time_stamps[$filename].' seconds)');
+        echoLine(formatTextColor('Pass ('.$source_time_stamps[$filename].' seconds) - '.$result['source'].$desc, 'green'));
 	} else {
-		echoLine($filename.' - '.$desc.'Fail ('.$source_time_stamps[$filename].' seconds)');
+		echoLine(formatTextColor('FAIL ('.$source_time_stamps[$filename].' seconds) - '.$result['source'].$desc, 'red'));
 	}
 }
