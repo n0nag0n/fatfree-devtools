3dea18f7b031cc6203c30e686b9361fc13946fcf
n0nag0n <acollier21@gmail.com>
2020-12-03 15:45:56 -0700

    added comments

--- templates/services.php
+++ templates/services.php
@@ -3,9 +3,11 @@
 // Setup Jig DB Connection
 $fw->set('DB', new \DB\Jig(PROJECT_DEVTOOLS_DATA_DIR, \DB\Jig::FORMAT_JSON ), 6000);
 </check><check if="{{ @cnf_mysql_name }}">
+// Setup MySQL DB Connection
 $fw->set('DB', new DB\SQL('mysql:host='.$fw->get('mysql.host').';port='.$fw->get('mysql.port').';dbname='.$fw->get('mysql.database').';charset='.$fw->get('mysql.charset'), $fw->get('mysql.username'),$fw->get('mysql.password')
 ));
 </check><check if="{{ @cnf_sqlite }}">
+// Setup SQLite DB Connection
 $fw->set('DB', new DB\SQL('sqlite:{{ @PROJECT_BASE_DIR }}{{ @cnf_sqlite }}'));
 </check>
 
