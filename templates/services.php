ba12af294fa6b78baf43d9cf7eac2b387f7aa033
n0nag0n <acollier21@gmail.com>
2020-12-03 15:45:17 -0700

    changed mysql configs in services to be vars, not hard coded

--- templates/services.php
+++ templates/services.php
@@ -3,8 +3,7 @@
 // Setup Jig DB Connection
 $fw->set('DB', new \DB\Jig(PROJECT_DEVTOOLS_DATA_DIR, \DB\Jig::FORMAT_JSON ), 6000);
 </check><check if="{{ @cnf_mysql_name }}">
-$fw->set('DB', new DB\SQL('mysql:host={{ @cnf_mysql_host }};port={{ @cnf_mysql_port }};dbname={{ @cnf_mysql_name }};charset={{ @cnf_mysql_charset }}', '{{ @cnf_mysql_username }}',
-    '{{ @cnf_mysql_password }}'
+$fw->set('DB', new DB\SQL('mysql:host='.$fw->get('mysql.host').';port='.$fw->get('mysql.port').';dbname='.$fw->get('mysql.database').';charset='.$fw->get('mysql.charset'), $fw->get('mysql.username'),$fw->get('mysql.password')
 ));
 </check><check if="{{ @cnf_sqlite }}">
 $fw->set('DB', new DB\SQL('sqlite:{{ @PROJECT_BASE_DIR }}{{ @cnf_sqlite }}'));
