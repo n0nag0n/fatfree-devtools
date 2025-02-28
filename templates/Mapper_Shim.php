f37be0ba0950ab5cd4e127b7d6854f00d3177770
dantefff <dantefff@users.noreply.github.com>
2025-02-28 15:03:33 +0100

    Update Mapper_Shim.php to fix #20
    As suggested by @n0nag0n , use #[\ReturnTypeWillChange] instead the new type to be retro compatible.

--- templates/Mapper_Shim.php
+++ templates/Mapper_Shim.php
@@ -4,6 +4,7 @@
  * This allows all mappers to be serializable with json
  */
 class Mapper_Shim extends Db\SQL\Mapper implements JsonSerializable {
+	#[\ReturnTypeWillChange]
 	public function jsonSerialize() {
 		return $this->cast();
 	}
